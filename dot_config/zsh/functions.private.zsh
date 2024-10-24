d20c460f237c297b14d085e002c3ac9f32544703
Rui Valim <r.valim.junior@gmail.com>
2024-10-24 13:59:44 -0300

    Delete dot_config/zsh/functions.private.zsh

--- dot_config/zsh/functions.private.zsh
+++
@@ -1,9 +0,0 @@
-azure_crefaz () {
-  export AZURE_CONFIG_DIR=~/.azure-crefaz/
-  az login --use-device-code
-}
-
-azure_vivo () {
-  export AZURE_CONFIG_DIR=~/.azure-vivo/
-  az login --use-device-code
-}
