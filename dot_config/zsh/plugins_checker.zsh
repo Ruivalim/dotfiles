d5b8cb22856a095834590031a01ec0dccc29518f
Rui Valim <r.valim.junior@gmail.com>
2024-11-04 14:12:02 -0300

    Update .config/zsh/plugins_checker.zsh Update .zshrc

--- dot_config/zsh/plugins_checker.zsh
+++ dot_config/zsh/plugins_checker.zsh
@@ -14,3 +14,12 @@ zsh_as="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 if ! path_exists "$zsh_as"; then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
+
+# ZSH NVM
+zsh_nvm="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm"
+
+if ! path_exists "$zsh_nvm"; then
+  git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+fi
+
+
