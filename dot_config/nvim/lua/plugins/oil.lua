b62383fda2039b061a95648389493543654ba49d
Rui Valim <r.valim.junior@gmail.com>
2024-11-11 16:32:33 -0300

    Update .config/nvim/lua/plugins/oil.lua

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,19 @@
 table.insert(plugins, {
 	"stevearc/oil.nvim",
 	opts = {
+		columns = {
+			"icon",
+			"size",
+			"mtime",
+		},
 		win_options = {
 			signcolumn = "yes:2",
 		},
 		view_options = {
 			show_hidden = true,
+		},
+		float = {
+			preview_split = "right",
 		},
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
