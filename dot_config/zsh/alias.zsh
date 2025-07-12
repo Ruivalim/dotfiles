5fae98b1c66a712d6075e451a2494a173ee2b080
Rui Valim <r.valim.junior@gmail.com>
2025-07-12 15:33:46 -0300

    new function

--- dot_config/zsh/alias.zsh
+++ dot_config/zsh/alias.zsh
@@ -4,6 +4,7 @@ else
   alias ls="ls -lah"
 fi
 alias work="cd ~/work"
+alias config="cd ~/.config"
 alias finder="open -a Finder ."
 if [ $TERM = "xterm-kitty" ]; then
   alias ssh="kitty +kitten ssh"
